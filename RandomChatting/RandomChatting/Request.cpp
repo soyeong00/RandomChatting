fd752e8acebd253effe2822c770971f57356286e
naviworks16 <naviworks16@gmail.com>
2020-05-14 15:35:56 +0900

    WaitForInvite added

--- RandomChatting/RandomChatting/Request.cpp
+++ RandomChatting/RandomChatting/Request.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+//#include "pch.h"
 #include "Request.h"
 #include <iostream>
 #include <WS2tcpip.h>
@@ -26,7 +26,7 @@ void Request::SendRequestToAnyone()
 	sockaddr_in server;
 	server.sin_family = AF_INET; //AF_INET =IPv4 address 를 쓰겠다고 해주는거
 	server.sin_port = htons(54000); //포트 지정
-	PCSTR ip = "192.168.43.255";
+	PCSTR ip = "192.168.43.255"; //접속해있는 ip 
 	inet_pton(AF_INET, ip, &server.sin_addr);//string을 byte array로 변환 해주는 방법
 
 	//Socket만들기 :Socket의 형은 datagram
@@ -71,4 +71,45 @@ void Request::SendRequestByName(string name)
 	closesocket(out);
 	//close down Winsock
 	WSACleanup();
+}
+
+void Request::WaitForInvite()
+{
+	WSADATA data;
+	WORD version = MAKEWORD(2, 2);
+	int wsOk = WSAStartup(version, &data);
+	if (wsOk != 0)
+	{
+		return;
+	}
+	SOCKET in = socket(AF_INET, SOCK_DGRAM, 0);
+	sockaddr_in serverHint;
+	serverHint.sin_addr.S_un.S_addr = ADDR_ANY; // Us any IP address available on the machine
+	serverHint.sin_family = AF_INET; // Address format is IPv4
+	serverHint.sin_port = htons(54000); // Convert from little to big endian
+	if (bind(in, (sockaddr*)&serverHint, sizeof(serverHint)) == SOCKET_ERROR)
+	{
+		return;
+	}
+	sockaddr_in client; // Use to hold the client information (port / ip address)
+	int clientLength = sizeof(client); // The size of the client information
+	char buf[1024];
+	while (true)
+	{
+		ZeroMemory(&client, clientLength); // Clear the client structure
+		ZeroMemory(buf, 1024); // Clear the receive buffer
+
+		// Wait for message
+		int bytesIn = recvfrom(in, buf, 1024, 0, (sockaddr*)&client, &clientLength);
+		//잘 받아졌는지 확인하는 단계
+		if (bytesIn == SOCKET_ERROR)
+		{
+		}
+		// Display message and client info
+		char clientIp[256]; // Create enough space to convert the address byte array
+		ZeroMemory(clientIp, 256); // to string of characters
+
+		// Convert from byte array to chars
+		inet_ntop(AF_INET, &client.sin_addr, clientIp, 256);
+	}
 }
--- RandomChatting/RandomChatting/Request.h
+++ RandomChatting/RandomChatting/Request.h
@@ -9,6 +9,5 @@ public:
 	//초대를 기다리는 펑션 returns the ip주소 of inviter
 	void SendRequestToAnyone(); //아무한테나 초대하기
 	void SendRequestByName(string name); //이름을 같이 넘겨주는 방법
-	//void WaitForRequest();	// 메인화면으로 넘어가자마자 시작 될, 초대를 기다리는 기능
-
+	void WaitForInvite();
 };
