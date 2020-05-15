8e3a753cd83f09949acc5001645cb8f52684b922
shwzang <shwzang@nate.com>
2020-05-15 09:54:26 +0900

    no message

--- RandomChatting/RandomChatting/Resource.h
+++ RandomChatting/RandomChatting/Resource.h
@@ -22,17 +22,12 @@
 #define IDC_STATIC_INV                  1012
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        135
 #define _APS_NEXT_COMMAND_VALUE         32771
-<<<<<<< HEAD
-#define _APS_NEXT_CONTROL_VALUE         1005
-#define _APS_NEXT_SYMED_VALUE           105
-=======
 #define _APS_NEXT_CONTROL_VALUE         1013
 #define _APS_NEXT_SYMED_VALUE           108
->>>>>>> 95628c37ab55d039bee66811c1f852098ba72c9a
 #endif
 #endif
